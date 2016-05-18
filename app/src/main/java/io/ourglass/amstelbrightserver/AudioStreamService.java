4d4531800bfdf310910bb4eb827a7ee2ce456ed0
atorres <torres.lyssa@gmail.com>
2016-05-18 13:46:08 -0700

    working on audio streaming

--- app/src/main/java/io/ourglass/amstelbrightserver/AudioStreamService.java
+++ app/src/main/java/io/ourglass/amstelbrightserver/AudioStreamService.java
@@ -3,16 +3,24 @@ package io.ourglass.amstelbrightserver;
 import android.app.Service;
 import android.content.Intent;
 import android.media.AudioFormat;
+import android.media.AudioManager;
 import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.media.MediaRecorder;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
 /**
@@ -22,109 +30,59 @@ public class AudioStreamService extends Service {
 
     static final String TAG = "AudioStreamService";
 
-    static final String DEFAULT_INET_ADDR = "224.0.0.3";
+    static final String DEFAULT_INET_ADDR = "207.62.163.38"; // sending directly to test tablet IP
+    //static final String DEFAULT_INET_ADDR = "224.0.0.3";
     static final int DEFAULT_PORT = 8888;
     static final int DEFAULT_TTL = 12;
 
-    static final int PACKET_SIZE = 1200;
-    static final int BUFFER_SIZE = 4096;
-
-    static final int SAMPLE_RATE = 32000;
-    static final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
-    static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
-    static final int SAMPLE_SIZE_BITS = 16;
-    static final int CHANNELS = 2;
-    static final boolean SIGNED = true;
-    static final boolean BIG_ENDIAN = false;
+    static final int SAMPLE_INTERVAL = 20; // milliseconds
+    static final int SAMPLE_SIZE = 2; // bytes per sample
+    static final int BUF_SIZE = SAMPLE_INTERVAL*SAMPLE_INTERVAL*SAMPLE_SIZE*2;
 
     String mAddr;
     int mPort;
     int mTTL;
     InetAddress mInetAddr = null;
-    MulticastSocket mSocket = null;
     Boolean mStreaming = false;
-    AudioFormat mAudioFormat = null;
 
-    int minBufSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
-    AudioRecord mRecorder = null;
-
-    private void startStreaming() {
-        Log.d(TAG, "startStreaming");
+    public void stream() {
         mStreaming = true;
 
         Thread streamThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                while (mStreaming) {
-                    sendAudioPacket();
+                int bytes_count = 0;
+                byte[] buf = new byte[BUF_SIZE];
+
+                try {
+                    mInetAddr = InetAddress.getByName(mAddr);
+                    DatagramSocket sock = new DatagramSocket();
+                    InputStream audio_stream = getResources().openRawResource(
+                            getResources().getIdentifier("sample", "raw", getPackageName()));
+
+                    while (mStreaming) {
+                        int bytes_read = audio_stream.read(buf, 0, BUF_SIZE);
+                        DatagramPacket pack = new DatagramPacket(buf, bytes_read,
+                                mInetAddr, mPort);
+                        sock.send(pack);
+                        bytes_count += bytes_read;
+                        Log.d(TAG, "bytes_count : " + bytes_count);
+                        Thread.sleep(SAMPLE_INTERVAL, 0);
+                    }
                 }
-                if (mSocket != null) {
-                    mSocket.close();
+                catch (Exception e) {
+                    Log.e(TAG, e.getLocalizedMessage());
                 }
             }
         });
 
         streamThread.start();
-
     }
 
-    private void stopStreaming() {
-        Log.d(TAG, "stopStreaming");
+    private void stopStream() {
+        Log.d(TAG, "stopStream");
         mStreaming = false;
-        mRecorder.release();
     }
-
-    private void sendAudioPacket() {
-        if (mInetAddr == null) {
-            try {
-                mInetAddr = InetAddress.getByName(mAddr);
-            } catch (UnknownHostException e) {
-                Log.e(TAG, e.getLocalizedMessage());
-                return;
-            }
-        }
-
-        if (mSocket == null || mSocket.isClosed()) {
-            try {
-                mSocket = new MulticastSocket(mPort);
-                mSocket.joinGroup(mInetAddr);
-                mSocket.setTimeToLive(mTTL);
-            } catch (IOException e) {
-                Log.e(TAG, e.getLocalizedMessage());
-                return;
-            }
-        }
-
-        if (mRecorder == null) {
-            mRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE, CHANNEL_CONFIG,
-                    AUDIO_FORMAT, minBufSize * 10);
-            mRecorder.startRecording();
-        }
-
-        byte[] buffer = new byte[minBufSize];
-
-        minBufSize = mRecorder.read(buffer, 0, buffer.length);
-
-        try {
-            mSocket.send(new DatagramPacket(buffer, buffer.length, mInetAddr, mPort));
-        } catch (IOException e) {
-            Log.e(TAG, e.getLocalizedMessage());
-        }
-
-        Log.d(TAG, "sent audio packet");
-    }
-
-    /*private AudioFormat getAudioFormat() {
-        if (mAudioFormat == null) {
-            AudioFormat.Builder ab = new AudioFormat.Builder();
-            ab.setSampleRate(SAMPLE_RATE);
-            ab.setEncoding(AUDIO_FORMAT);
-            ab.setChannelMask(CHANNEL_CONFIG);
-            mAudioFormat = ab.build();
-        }
-
-        return mAudioFormat;
-    }*/
 
     @Override
     public void onCreate() {
@@ -149,7 +107,7 @@ public class AudioStreamService extends Service {
             mPort = DEFAULT_PORT;
         }
 
-        startStreaming();
+        stream();
 
         return Service.START_STICKY;
     }
@@ -157,7 +115,7 @@ public class AudioStreamService extends Service {
     @Override
     public void onDestroy() {
         Log.d(TAG, "onDestroy");
-        stopStreaming();
+        stopStream();
     }
 
     @Override
